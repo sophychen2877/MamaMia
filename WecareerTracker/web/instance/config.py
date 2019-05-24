7fbda2f9efc494ee18e4397be48eefc14bf1a3fc
sophychen2877 <sophychen2877@gmail.com>
2019-05-24 16:35:24 -0400

    Update config.py

--- WecareerTracker/web/instance/config.py
+++ WecareerTracker/web/instance/config.py
@@ -1,6 +1,6 @@
 #SECRET_KEY and database with password connection (and any other API keys) need to be kept in the instance file, which is not subject to version control
 #these configuration settings are used in dev environment
-SECRET_KEY = 'you-will-never-guess'
+SECRET_KEY = ''
 
 import os
 POSTGRES_DATABASE = os.environ['POSTGRES_DATABASE']
@@ -9,11 +9,6 @@ POSTGRES_PASSWORD = os.environ['POSTGRES_PASSWORD']
 POSTGRES_HOST = os.environ['POSTGRES_HOST']
 POSTGRES_PORT = os.environ['POSTGRES_PORT']
 
-# USER='postgres'
-# PASSWORD='postgres'
-# HOST='postgres'
-# DB='postgres1234'
-# PORT='5432'
 
 #use postgresql
 SQLALCHEMY_DATABASE_URI = f'postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_HOST}:{POSTGRES_PORT}/{POSTGRES_DATABASE}'
